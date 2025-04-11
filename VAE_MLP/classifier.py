38f54e8b70b1924bc87e540909234e604b1a34d2
Moksha1512 <127943099+Moksha1512@users.noreply.github.com>
2025-04-11 11:58:23 +0530

    Update classifier.py

--- VAE_MLP/classifier.py
+++ VAE_MLP/classifier.py
@@ -1,15 +1,10 @@
 import torch.nn as nn
 
-# Defining the structure of a classifier
-
 class Classifier(nn.Module):
 
   def __init__(self, in_features):
 
     super(Classifier,self).__init__()
-
-    # Block of fully Connected layers for classification
-
     self.FullyConnectedBlock = nn.Sequential(
         nn.Linear(in_features,128),
         nn.ReLU(),
